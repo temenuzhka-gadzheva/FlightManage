ddd2bcb578c8b1aec48401956e43dece49703f4f
temenujka_211 <temenujka_211@abv.bg>
2020-02-22 20:17:23 +0200

    Override Edit.cshtml and Index.cshtml

--- FlightManage/Data/ReservationDbContext.cs
+++ FlightManage/Data/ReservationDbContext.cs
@@ -12,5 +12,10 @@ namespace FlightManage.Data
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Flight> Flights { get; set; }
         public virtual DbSet<Reservation> Reservations { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-UF96G8T\SQLEXPRESS;Initial Catalog=MyLibraryDb; Trusted_Connection=true ");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
     }
 }
